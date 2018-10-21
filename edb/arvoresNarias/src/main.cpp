659a180295d493904d805672b3aa7561ffc2d157
Epitacio <bessa.epitacio@gmail.com>
2018-10-21 18:35:19 -0300

    Melhorando interação do usuário

--- edb/arvoresNarias/src/main.cpp
+++ edb/arvoresNarias/src/main.cpp
@@ -3,28 +3,33 @@
 #include "../include/arvoreComNos.hpp"
 
 int main() {
-    ArvoreComNos<int> arvore(2);
+    int N;
+    std::cout << "Insira o valor de N da árvore: ";
+    std::cin >> N;
+    ArvoreComNos<int> arvore(N);
     int option = 0;
-    std::cin >> option;
 
-    while(option != -1) {
-        arvore.addElemento(option);
+    do {
+        std::cout << "\n1 para inserir;\n"
+                << "2 para buscar;\n"
+                << "3 para remover;\n"
+                << "4 para imprimir;\n" 
+                << "0 para sair;\n";
         std::cin >> option;
-    }
-    arvore.print();
-    std::cin >> option;
 
-    while(option != -1) {
-        std::cout << '\n' << (arvore.busca(option) != NULL ? "true" : "false") << '\n';
-        std::cin >> option;
-    }
+        if(option >= 1 && option <=3) {
+            int elemento;
+            std::cin >> elemento;
 
-    arvore.print();
-    std::cin >> option;
-
-    while(option != -1) {
-        arvore.remover(option);
-        arvore.print();
-        std::cin >> option;
-    }
+            if(option == 1) {
+                arvore.addElemento(elemento);
+            } else if(option == 2) {
+                std::cout << (arvore.busca(elemento) != NULL ? "Está" : "Não está") << " na árvore\n";
+            } else {
+                arvore.remover(elemento);
+            }
+        } else if(option == 4) {
+            arvore.print();
+        }
+    } while(option != 0);
 }
