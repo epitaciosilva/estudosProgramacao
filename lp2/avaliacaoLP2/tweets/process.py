586e1284f6d8624c9dd1a3ac4cf30ba6cf049e62
Epitácio <bessa.epitacio@gmail.com>
2018-10-24 10:16:07 -0300

    Ajustes no regex na questão dos Tweets

--- lp2/avaliacaoLP2/tweets/process.py
+++ lp2/avaliacaoLP2/tweets/process.py
@@ -1,13 +1,12 @@
 from contextlib import redirect_stdout
-import re
+from re import findall
 
 with open("tweets.in", encoding="utf-8") as f:
-    tweets = [line.strip() for line in f.readlines()]
-    tweets.sort()
+	tweets = [line.strip() for line in f.readlines()]
+	tweets.sort()
 
 with open("tweets.out", "w", encoding="utf-8") as f:
-    with redirect_stdout(f):
-        for tweet in tweets:
-            fake_news = list(re.finditer("(RT @\w+)|(http[s]?\S+)", tweet))
-            if len(fake_news) > 1 and "RT" in fake_news[0].group():
-                print("{} {}".format(fake_news[0].group().split()[1], fake_news[1].group()))
+	with redirect_stdout(f):
+		for tweet in tweets:
+			for user,url in findall(r"RT (@\w+).*(http[s]?\S+).*", tweet):
+				print("{} {}".format(user, url))
