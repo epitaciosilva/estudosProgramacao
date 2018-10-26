1c0420cfd2387fc927d79ccf3f3b87da5a973edb
Epitácio <bessa.epitacio@gmail.com>
2018-10-26 11:09:50 -0300

    Criando class HeapBinaria

--- edb/heapBinaria/heap.py
+++ edb/heapBinaria/heap.py
@@ -1,73 +1,72 @@
 from copy import copy 
-arvore = []
 
-def pai(index):
-    return (index-1)//2
+class HeapBinaria:
+    def __init__(self):
+        self.arvore = []
 
-def filhoEsquerdo(index):
-    return index*2+2
+    def pai(self, index):
+        return (index-1)//2
 
-def filhoDireito(index):
-    return index*2+1
+    def filhoEsquerdo(self, index):
+        return index*2+2
 
-def swap(index1, index2):
-    global arvore
-    value = copy(arvore[index1])
-    arvore[index1] = copy(arvore[index2])
-    arvore[index2] = value
+    def filhoDireito(self, index):
+        return index*2+1
 
-def remove(index1, index2):
-    arvore[index1] = copy(arvore[index2])
-    arvore.pop(index2)
+    def swap(self, index1, index2):
+        value = copy(self.arvore[index1])
+        self.arvore[index1] = copy(self.arvore[index2])
+        self.arvore[index2] = value
 
-def heapify(index):
-    global arvore
+    def remove(index1, index2):
+        self.arvore[index1] = copy(self.arvore[index2])
+        self.arvore.pop(index2)
 
-    esquerdo = filhoEsquerdo(index)
-    direito = filhoDireito(index)
-    maior = 0
+    def heapify(self, index):
+        esquerdo = self.filhoEsquerdo(index)
+        direito = self.filhoDireito(index)
+        maior = 0
 
-    if esquerdo <= len(arvore)-1 and arvore[esquerdo] < arvore[index]:
-        maior = esquerdo
-    else:
-        maior = index
+        if esquerdo <= len(self.arvore)-1 and self.arvore[esquerdo] < self.arvore[index]:
+            maior = esquerdo
+        else:
+            maior = index
 
-    if direito <= len(arvore)-1 and arvore[direito] < arvore[maior]:
-        maior = direito
+        if direito <= len(self.arvore)-1 and self.arvore[direito] < self.arvore[maior]:
+            maior = direito
 
-    if maior != index:
-        swap(index, maior)
-        heapify(maior)
+        if maior != index:
+            self.swap(index, maior)
+            self.heapify(maior)
 
-def pop():
-    if len(arvore) == 0:
-        return
-    elif len(arvore) == 1:
-        arvore.pop(0)
-        return
+    def pop(self):
+        if len(self.arvore) == 0:
+            return
+        elif len(self.arvore) == 1:
+            self.arvore.pop(0)
+            return
 
-    remove(0, len(arvore)-1)
-    contruirHeapify()
+        self.remove(0, len(self.arvore)-1)
+        self.contruirHeapify()
 
-def push(value):
-    global arvore
-    arvore.append(value)
-    contruirHeapify()
+    def push(self,value):
+        self.arvore.append(value)
+        self.contruirHeapify()
 
-def contruirHeapify():
-    i = pai(len(arvore)-1)
-    
-    while i >= 0:
-        heapify(i)
-        i -= 1
+    def contruirHeapify(self):
+        i = self.pai(len(self.arvore)-1)
+        
+        while i >= 0:
+            self.heapify(i)
+            i -= 1
+    def print(self):
+        print(self.arvore)
 
-push(20)
-push(10)
-push(5)
-push(8)
-push(3)
-push(30)
-print(arvore)
-pop()
-print(arvore)
-
+heap = HeapBinaria()
+heap.push(20)
+heap.push(10)
+heap.push(5)
+heap.push(8)
+heap.push(3)
+heap.push(30)
+heap.print()
